9480bd8104087995ed77a05a3e6bd8b7db7c4893
Oleg Tverskoy <otverskoj@gmail.com>
2022-03-31 14:16:43 +0600

    Add simple pipeline in main.py

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
-def main(input_video_path: str) -> None:
-    print(input_video_path)    
+import cv2
+import numpy as np
+
+
+def main(input_video_path: str,
+         output_video_path: str) -> None:
+    video_capture = cv2.VideoCapture(input_video_path)
+
+    # person_detector = PersonDetector()
+    # person_embedderer = PersonEmbedderer()
+    # embedding_storage = EmbeddingStorage()
+
+    person_counter, frames_counter = 0, 0
+
+    out_resolution = 1920, 1080
+    writer = cv2.VideoWriter(output_video_path,
+                             cv2.VideoWriter_fourcc(*'mp4v'),
+                             25, 
+                             out_resolution,
+                             True)
+
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+
+        # persons = person_detector.detect(frame)
+        # person_embeddings = person_embedderer.embed(persons)
+
+        # stored_person_embeddings = embedding_storage.person_embeddings()
+        # similarity_matrix = LinalgUtils.count_similarity(stored_person_embeddings,
+        #                                                  person_embeddings,
+        #                                                  metric='cosine')
+        
+        # # ищем индексы строки значения, являющего максимумов в столбце
+        # indices_to_swap = LinalgUtils.swap_indices(similarity_matrix, 
+        #                                            metric='cosine')
+        # embedding_storage.update_embeddings(person_embeddings, indices_to_swap)
+        
+        # # индексы строк новых для нас людей
+        # indices_to_add = np.array(
+        #     filter(
+        #         lambda i: i not in indices_to_swap, 
+        #         range(person_embeddings.shape[1])
+        # ))
+        # embedding_storage.add_embeddings(person_embeddings)
+
+        # person_counter += indices_to_add.shape[0]
+
+        # data = tuple(
+        #     {
+        #         'type': 'text',
+        #         'coords': (),
+        #         'content': f'Total persons: {person_counter}'
+        #     }
+        # )
+        # frame_to_write = DrawUtils.draw(data, frame[:])
+
+        frame_to_write = cv2.resize(frame, out_resolution)
+        if frames_counter % 125 == 0:
+            print(frames_counter)
+        writer.write(frame_to_write)
+        frames_counter += 1
 
 
 if __name__ == '__main__':
-    main()
+    main(r'data\input\video\clip.mp4', r'data\output\video\clip.mp4')
